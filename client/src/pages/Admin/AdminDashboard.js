6eb967391cdfb5ac7f87a8d47571b3d466b6108d
pushpraj paroha <satyamparoha786@gmail.com>
2023-10-26 10:10:24 +0530

    admindashboardchanges

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -11,6 +11,7 @@ import AllFood from "./AllFood";
 import ReportsAdmin from "./ReportsAdmin";
 import EmployeeRecord from "./EmployeeRecord";
 import { IoFastFood,IoLogOut } from "react-icons/io5";
+import HistoryHotelOrder from "../../components/HIstoryHotelOrder";
 
 const Branch = () => {
   const [auth, setAuth] = useAuth();
@@ -64,6 +65,7 @@ const Branch = () => {
     { name: "food", id: "foodid" },
     { name: "reports", id: "reportsid" },
     { name: "employeeRecords", id: "employeeRecordsid" },
+    { name: "HistoryHotelOrder", id: "HistoryHotelOrderId" },
     {
       name: "Logout",
       id: "logoutId",
@@ -188,6 +190,32 @@ const Branch = () => {
                 Home
               </span>
             </Link>
+          </li>
+          <li
+            className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 ${"mt-2"} `}
+            onClick={() => {
+              handleMenuItemClick("HistoryHotelOrderId");
+              setOpen(!open);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+              />
+            </svg>
+
+            <span className={`${!open && "hidden"} origin-left duration-200`}>
+              Your Personal Hotel Order History 
+            </span>
           </li>
           <li
             className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 ${"mt-2"} `}
@@ -487,6 +515,7 @@ const Branch = () => {
             {selectedContent === "foodid" && <AllFood />}
             {selectedContent === "reportsid" && <ReportsAdmin />}
             {selectedContent === "employeeRecordsid" && <EmployeeRecord />}
+            {selectedContent === "HistoryHotelOrderId" && <HistoryHotelOrder/>}
           </main>
         </div>
       </div>
