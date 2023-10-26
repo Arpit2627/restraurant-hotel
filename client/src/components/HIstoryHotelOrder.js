748c175232c99450edd0c6b9e5661311c0dc2017
pushpraj paroha <satyamparoha786@gmail.com>
2023-10-26 16:08:55 +0530

    hho

--- client/src/components/HIstoryHotelOrder.js
+++ client/src/components/HIstoryHotelOrder.js
@@ -1,4 +1,23 @@
-import React, { useState, useEffect } from "react";
+[15:34, 10/26/2023] Versha Mst: import React from "react";
+import Layout from "../../components/Layout/Layout";
+import { useAuth } from "../../context/auth";
+import HistoryHotelOrder from "../../components/HIstoryHotelOrder"; 
+const Dashboard = () => {
+  const [auth] = useAuth();
+
+  return (
+    <Layout title={"Dashboard - Restaurant"}>
+      <div className="bg-gray-100">
+        <div className="container mx-auto p-4">
+          <h1 className="text-3xl font-semibold my-20 text-center mb-6">Welcome, {auth?.user?.name}!</h1>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="bg-white rounded-lg shadow p-4">
+            <h2 className="text-3xl font-bold mb-4 text-center text-pink-300">User Information</h2>
+
+              <div className="mb-2 mx-8">
+…
+[15:34, 10/26/2023] Versha Mst: import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useAuth } from "../context/auth";
 import toast from "react-hot-toast";
